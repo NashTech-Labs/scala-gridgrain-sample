ee8851653626e728be3a97e734417955271c5cf8
Shivansh <shiv4nsh@gmail.com>
2016-08-22 16:21:16 +0530

    Refactored the code to run the IgniteCallable

--- src/main/scala/StarterApplication.scala
+++ src/main/scala/StarterApplication.scala
@@ -10,14 +10,23 @@ import scala.collection.JavaConversions._
 object StarterApplication extends App {
 
   try {
+    // Ignition.setClientMode(true)
     val ignite = Ignition.start("src/main/resources/example-ignite.xml")
     val calls = new util.ArrayList[IgniteCallable[Int]]()
-    val word = "Count characters using callable".split(" ").map(a => calls(a.length))
+    //val word: Array[IgniteCallable[Int]] = "Count characters using callable".split(" ").map(a => calls(a.length))
+
+    for (word <- "Count characters using callable".split(" ")) {
+      calls.add(new IgniteCallable[Int] {
+        override def call(): Int = word.length
+      })
+    }
     // Iterate through all the words in the sentence and create Callable jobs.
     // Execute collection of Callables on the grid.
     val res = ignite.compute().call(calls)
-    // Add up all the results.
+    // Add up all the results
+    // .
     val sum = res.toList.sum
+
     println("Total number of characters is '" + sum + "'.");
   } catch {
     case e: Exception =>
